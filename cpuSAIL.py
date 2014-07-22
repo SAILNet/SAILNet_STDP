0592166ff881ba0c4c74a0d9880ac7a20b2934d6
bernaljg <bernaljg@berkeley.edu>
2014-07-21 17:54:29 -0700

    Fixing Merge Errors
    This change fixes the errors produced by my successful attempt at fixing
    local errors.

--- cpuSAIL.py
+++ cpuSAIL.py
@@ -6,27 +6,18 @@ def activities(X,Q,W,theta):
     batch_size, N = X.shape
     sz = int(np.sqrt(N))
 
-<<<<<<< HEAD
     M = Q.shape[1]
-=======
-    M = Q.shape[1] 
     
     """
     Q is the matrix of connection strengths from each input to each neuron. it is a inputs X number of neurons
     """
->>>>>>> 463e8041f43a7b764ab39c2be56398ee258b6131
 
     num_iterations = 50
 
     eta = .1
-<<<<<<< HEAD
 
     B = X.dot(Q)
-=======
-    
-    B = X.dot(Q)
     #weighting the input activity by the feed-forward weights
->>>>>>> 463e8041f43a7b764ab39c2be56398ee258b6131
 
     T = np.tile(theta,(batch_size,1))
 
@@ -37,26 +28,17 @@ def activities(X,Q,W,theta):
     """    
     aas determines who spikes. Subtracting aas.dot(W) creates inhibition based on the weight.
     aas is either 1 or 0, either fired or not.
-    
-<<<<<<< HEAD
-=======
+
     (1 - eta)*Ys is a decay term.
     
     eta*(B) is a term that increases the activity based on the strength of the input
     weighted by the feed forward weights.
     
     eta*aas.dot(W) term is the inhibitory term.    
->>>>>>> 463e8041f43a7b764ab39c2be56398ee258b6131
     """
     for tt in xrange(num_iterations):
         Ys = (1.-eta)*Ys+eta*(B-aas.dot(W))
         aas = np.zeros((batch_size,M))
-<<<<<<< HEAD
-        aas[Ys > T] = 1.
-        Y += aas
-        Ys[Ys > T] = 0.
-
-=======
         #This resets the current activity of the time step to 0's        
         aas[Ys > T] = 1.
         #If the activity of a given neuron is above the threshold, set it to 1 a.k.a. fire.
@@ -64,8 +46,7 @@ def activities(X,Q,W,theta):
         #update total activity
         Ys[Ys > T] = 0.
         #after firing set back to zero for activity calculations in next time step
-        
->>>>>>> 463e8041f43a7b764ab39c2be56398ee258b6131
+
     return Y
 
 rng = np.random.RandomState(0)
@@ -85,27 +66,16 @@ BUFF = 20
 # Neuron Parameters
 N = 256
 sz = np.sqrt(N).astype(np.int)
-<<<<<<< HEAD
-OC = 2
-M = OC*N
-
-# Network Parameters
-p = .05
-=======
 OC = 2 #Over-Completeness: num of neurons = OC * num of inputs
 M = OC*N #M is the number of neurons
 
 # Network Parameters
 p = .05 #Sparcity
->>>>>>> 463e8041f43a7b764ab39c2be56398ee258b6131
 
 # Initialize Weights
 Q = rng.randn(N,M)
 Q = Q.dot(np.diag(1./np.sqrt(np.diag(Q.T.dot(Q)))))
-<<<<<<< HEAD
-=======
 #1./np.sqrt(np.diag(Q.T.dot(Q))) normalizes the Q matrix
->>>>>>> 463e8041f43a7b764ab39c2be56398ee258b6131
 W = np.zeros((M,M))
 theta = 2.*np.ones(M)
 
@@ -133,18 +103,12 @@ for tt in xrange(num_trials):
         r = BUFF+int((imsize-sz-2.*BUFF)*rng.rand())
         c = BUFF+int((imsize-sz-2.*BUFF)*rng.rand())
         myimage = images[int(num_images*rng.rand()),r:r+sz,c:c+sz].ravel()
-<<<<<<< HEAD
-        myimage = myimage-np.mean(myimage)
-        myimage = myimage/np.std(myimage)
-        X[ii] = myimage
-=======
         #takes a chunck from a random image, size of 16X16 patch at a random location       
         myimage = myimage-np.mean(myimage)
         myimage = myimage/np.std(myimage)
         #Forces mean to be 0
         X[ii] = myimage
         #creating a list of image patches to work with
->>>>>>> 463e8041f43a7b764ab39c2be56398ee258b6131
         
     dt = time.time()-dt
     data_time += dt/60.
