7aa9cca734b13c7786a7bdd304e2d83c9757d3df
GregMartinUCB <gmartin@berkeley.edu>
2014-09-22 21:46:39 -0700

    Reorganized all the images into a folder

--- Plotting/SAILnet_Plotting.py
+++ Plotting/SAILnet_Plotting.py
@@ -34,7 +34,7 @@ class Run():
         img = tile_raster_images(self.Q.T, img_shape = (side,side), tile_shape = (2*side,side*OC/2), tile_spacing=(1, 1), scale_rows_to_unit_interval=True, output_pixel_vals=True)
         plt.imshow(img,cmap=plt.cm.Greys)
         plt.title(name + 'Receptive Fields')
-        plt.imsave(name+'.png', img, cmap=plt.cm.Greys)
+        plt.imsave('Images\The '+name+'.png', img, cmap=plt.cm.Greys)
         plt.show()
         plt.close
         plt.clf
@@ -47,39 +47,41 @@ class Run():
         plt.legend(bbox_to_anchor=(1,.5))
         plt.title(title)
         plt.xlabel("Number of Trials")
-        plt.savefig('stdp_dW.png')
+        plt.savefig('Images\stdp_dW.png')
         plt.clf
         
     def Plotstdp(self,title):
         plt.plot(self.mag_stdp, color="green", label="STDP")
         plt.title(title)
         plt.xlabel("Number of Trials")
+        plt.savefig('Images\Magnitude STDP Using'+self.fileName[:len(self.fileName)-4]+'.png')
     
     def PlotdW(self,title):
         plt.plot(self.mag_dW,color="blue", label="dW")
         plt.title(title)
         plt.xlabel("Number of Trials")
+        plt.savefig('Images\Magnitude dW Using'+self.fileName[:len(self.fileName)-4]+'.png')
         
     def PlotYavg(self):
         
         plt.plot(self.Yavg, color="yellow")
         plt.title(self.fileName + 'Y_avg')
         plt.xlabel("Number of Trials")
-        plt.savefig(self.[:len(self.fileName)-4] + 'Y_avg.png')
+        plt.savefig('Images\Y_avg'+self.fileName[:len(self.fileName)-4]+'.png')
     
     def PlotCavg(self):
         
         plt.plot(self.Cavg, color="red")
         plt.title(self.fileName + 'C_avg')
         plt.xlabel("Number of Trials")
-        plt.savefig(self.fileName[:len(self.fileName)-4] + 'C_avg.png')
+        plt.savefig('Images\C_avg'+ self.fileName[:len(self.fileName)-4] + '.png')
         
     def Plotcor(self):
         
         plt.plot(self.correlation, color = "purple")
         plt.title(self.fileName + 'Correlation of dW and STDP')
         plt.xlabel("Number of Trials")
-        plt.savefig(self.fileName[:len(self.fileName)-4]  + 'Correlation of dW and STDP.png')
+        plt.savefig('Images\Correlation of dW and STDP'+self.fileName[:len(self.fileName)-4] +'.png')
 
 
 
@@ -105,9 +107,9 @@ plt.figure(stdp5000.PlotdWstdp("Magnitudes of dW and STDP Matricies Using STDP")
 
 plt.figure(dW5000.PlotdWstdp("Magnitudes of dW and STDP Matricies Using dW"))
 
-plt.figure(stdp5000.Plotstdp("Using Just STDP"))
+plt.figure(stdp5000.Plotstdp("Magnitude of "))
 
-plt.figure(stdp5000.PlotdW("Using Just STDP"))
+plt.figure(stdp5000.PlotdW("Magnitude of "))
 
 plt.figure(stdp5000.Plot_RF("RF Just STDP at 5000 Trials"))
 
