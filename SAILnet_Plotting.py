Fixed Normalization of Inhibitory Connection Hist
Used numpy.histogram to fix the normalization when bins are not unity

SAILnet_Plotting.py
@@ -112,7 +112,9 @@ class Plot():
         zeros = np.nonzero(W_flat == 0) #Locates zeros
         W_flat = np.delete(W_flat, zeros) #Deletes Zeros
         W_flat = np.abs(np.log(W_flat))
-        plt.hist(W_flat, bins = 100000, normed = True)
+        hist, bin_edges = np.histogram(W_flat, bins = 10000, density = True)
+        hist = np.append(np.array[0],hist)
+        plt.plot(bin_edges,hist,'o')
         plt.xlabel("Inhibitory Connection Strength")
         plt.ylabel("PDF log(connection strength)")
         plt.title("Histogram of Inhibitory Connection Strengths for 25000 Iterations and STDP Learning Rule")