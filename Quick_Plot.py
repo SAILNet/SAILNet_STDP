c7cc482527dc4d15d0344e56b14fa7bb299b52da
bernaljg <bernaljg@berkeley.edu>
2015-04-06 17:52:23 -0700

    Created Plots with SAILNet and STDP for poster

--- Quick_Plot.py
+++ Quick_Plot.py
@@ -11,7 +11,7 @@ This program lets us create new plots based on old Pickle files from previous ru
 from SAILnet_Plotting import Plot
 
 OverC = 8
-SA = 3
+SA = 2
 
 directory = './Trials/OC' + str(OverC) + '_' + str(SA)
 data_filename = directory + '/data.pkl'
